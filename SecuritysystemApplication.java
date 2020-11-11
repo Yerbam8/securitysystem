c1bfbd291961f6f6092453b664a8e19eed6e2632
Kamil <kamil.gruda.94@gmail.com>
2020-11-11 22:19:32 +0100

    Initial commit

---
+++ SecuritysystemApplication.java
@@ -0,0 +1,13 @@
+package com.kamilgruda.securitysystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecuritysystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SecuritysystemApplication.class, args);
+    }
+
+}
